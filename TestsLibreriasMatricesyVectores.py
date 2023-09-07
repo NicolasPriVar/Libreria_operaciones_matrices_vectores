023a88c18ccbf24eaf4d3b8a016ae121fd3d9d05
NicolasPriVar <142689710+NicolasPriVar@users.noreply.github.com>
2023-09-06 22:28:50 -0500

    Update TestsLibreriasMatricesyVectores.py

--- TestsLibreriasMatricesyVectores.py
+++ TestsLibreriasMatricesyVectores.py
@@ -86,5 +86,58 @@ class TestCibreriaComplejos(unittest.TestCase):
         v=lm.conMat([[2+2j,2-2j],[2+3j,8-2j]])
         self.assertAlmostEqual(v[0], [2-2j,2+2j])
         self.assertAlmostEqual(v[1], [2-3j,8+2j])
+    def test_dagaVector(self):
+        #Primer caso de prueba
+        v=lm.dagaVector([1+4j, 2-5j])
+        self.assertAlmostEqual(v, [(1-4j), (2+5j)])
+        #Segundo caso de prueba
+        v=lm.dagaVector([2+4j, 4-7j])
+        self.assertAlmostEqual(v, [(2-4j), (4+7j)])
+    def test_dagaMatriz(self):
+        #Primer caso de prueba
+        v=lm.dagaMatriz([[3+4j,2-5j],[4+3j,8-9j]])
+        self.assertAlmostEqual(v[0], [3-4j, 4-3j])
+        self.assertAlmostEqual(v[1], [2+5j, 8+9j])
+        #Segundo caso de prueba
+        v=lm.dagaMatriz([[5+7j,2-3j],[3+5j,2-3j]])
+        self.assertAlmostEqual(v[0], [5-7j, 3-5j])
+        self.assertAlmostEqual(v[1], [2+3j, 2+3j])
+    def test_multiplicacionMatrices(self):
+        #Primer caso de prueba
+        v=lm.multiMatrices([[3+5j, 5-8j],[2+5j, 5+5j]], [[3+5j, 2+5j],[5-8j, 5+5j]])
+        self.assertAlmostEqual(v, [[-55-50j, 46+10j],[46+10j, -21+70j]])
+        #Segundo caso de prueba
+        v=lm.multiMatrices([[3+4j,2-5j],[4+3j,8-9j]],[[5+7j,2-3j],[3+5j,2-3j]])
+        self.assertAlmostEqual(v, [[18+36j, 7-17j], [68+56j, 6-48j]])
+    def test_accionMatconVec(self):
+        #Primer caso de prueba
+        v=lm.accionMatVec([[3+4j,2-5j],[4+3j,8-9j]], [1+4j, 2-5j])
+        self.assertAlmostEqual(v[0], [(-34-4j)])
+        self.assertAlmostEqual(v[1], [(-37-39j)])
+        #Segundo caso de prueba
+        v=lm.accionMatVec([[5+7j,2-3j],[3+5j,2-3j]], [2+4j, 4-7j])
+        self.assertAlmostEqual(v[0], [(-31+8j)])
+        self.assertAlmostEqual(v[1], [(-27-4j)])
+    def test_productoInterno(self):
+        #Primer caso de prueba
+        v=lm.productoInterno([1, 2+3j, 6j],[0, 1j, 2+4j])
+        self.assertAlmostEqual(v, (27-10j))
+        #Segundo caso de prueba
+        v=lm.productoInterno([1+4j, 2-5j],[2+4j, 4-7j])
+        self.assertAlmostEqual(v, (61+2j))
+    def test_normaVec(self):
+        #Primer caso de prueba
+        v=lm.normaVec([1+4j, 2-5j])
+        self.assertAlmostEqual(v, (0.9867451347791263+6.080597500329245j))
+        #Segundo caso de prueba
+        v=lm.normaVec([2+4j, 4-7j])
+        self.assertAlmostEqual(v, (2.7575326737829853+7.252860569939335j))
+    def test_distanciaVec(self):
+        #Primer caso de prueba
+        v=lm.distaciasVectores([1+4j, 2-5j],[2+4j, 4-7j])
+        self.assertAlmostEqual(v, (2.1286448+1.8791298j))
+        #Segundo caso de prueba
+        v=lm.distaciasVectores([2+2j, 2-2j],[2+2j, 2-2j])
+        self.assertAlmostEqual(v, 0)
 if __name__ == '__main__':
     unittest.main()
